69b3c7d84611277ed9316dd482d3217f277ecb9c
czambelli <zmbl.soft@gmail.com>
2013-05-14 16:50:56 +0200

    Ends coding session

--- src/SuperHeroParser.php
+++ src/SuperHeroParser.php
@@ -6,62 +6,53 @@ class SuperHeroParser
 	 * @var DomDocument
 	 */
 	protected $xml;
-	
+
 	function __construct($filename = null)
 	{
 		if(!$filename)
 		{
 			$filename = "../data/superheroes.xml";
 		}
-		
-		$xml = new DomDocument();
-		
-		if(!$xml->load($filename))
+
+		$this->xml = new DomDocument();
+
+		if(!$this->xml->load($filename))
 		{
 			throw new AvengerException("Impossible d'analyser le fichier $filename");
 		}
-		
+
 		$this->parse();
 	}
-	
+
 	/**
-	 * Analyse le fichier XML chargé dans $xml et récupère une liste de super-héros.
+	 * Analyse le fichier XML chargé dans $this->xml et récupère une liste de super-héros.
 	 */
 	protected function parse()
 	{
-		$superheroes = $xml->getElementsByTagName('superhero');
-		
+		$superheroes = $this->xml->getElementsByTagName('superhero');
+
 		// Parcourt la liste des super-héros trouvés
 		foreach($superheroes as $superhero)
 		{
-			$sh = new SuperHero();
+			$character = new SuperHero();
 			
+			foreach($superhero->childNodes as $childNode)
+			{
+				if($childNode->nodeType == XML_ELEMENT_NODE)
+				{
+					foreach($childNode->childNodes as $node)
+					{
+						if($node->nodeType == XML_ELEMENT_NODE)
+						{
+							echo 'CONTENEUR';// $node;
+						}
+					}
+				
+					echo $childNode->nodeName . " => " . $childNode->nodeValue . "<br />";
+				}
+			}
 			
-			
-			$sh->nickname = $superhero->getElementsByTagName('nickname');
+			$character->nickname = $superhero->getElementsByTagName('nickname');
 		}
 	}
-	
-	protected function parseSubItems($parentNode, $character)
-	{
-		foreach($parentNode->childNodes as $node)
-		{
-			if($node->childNodes instanceof DOMNodeList and $node->length > 0)
-			{
-				// Le noeud contient des fils : tableau d'éléments
-				// On analyse tous les fils les uns après les autres
-				return $this->parseSubItems($node->nodeName, $node->$nodeValue);
-			}
-			else
-			{
-				// Le noeud ne contient pas de fils
-				// On envoie directement l'attribut dans le personnage
-				$character->addAttribute($node->nodeName, $node->nodeValue);
-			}
-		}
-	}
-	
-	public function 
-	
-	
 }
