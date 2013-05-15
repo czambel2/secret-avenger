512179fe64efd258466e337dbbe048f938844b72
czambelli <zmbl.soft@gmail.com>
2013-05-15 22:26:19 +0200

    Adds protected properties to SuperHero

--- src/SuperHero.php
+++ src/SuperHero.php
@@ -2,5 +2,23 @@
 
 class SuperHero extends SuperThing
 {
+	protected $nickname;
+	protected $firstName;
+	protected $lastName;
+	protected $universe;
+	protected $picture;
+	protected $superpowers;
+	protected $nemeses;
+	protected $lovers;
+	protected $sidekicks;
 
+	public function dump()
+	{
+		$v = array();
+		foreach($this as $key => $value)
+		{
+			$v[$key] = $value;
+		}
+		var_dump($v);
+	}
 }
