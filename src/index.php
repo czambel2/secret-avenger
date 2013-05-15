f573f2ecf8411913b2105062cab5775065549c4f
czambelli <zmbl.soft@gmail.com>
2013-05-15 22:26:36 +0200

    Adds dump in index.php

--- src/index.php
+++ src/index.php
@@ -2,4 +2,11 @@
 
 require_once 'autoload.php';
 
+if(php_sapi_name() != 'cli') echo '<pre style="font-family: Consolas, Inconsolata, monospace">';
+
 $parser = new SuperHeroParser();
+
+foreach($parser->characters as $character)
+{
+	$character->dump();
+}
