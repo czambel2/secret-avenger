4360d8f5c5051b6f5b98519d75023e8f23f999b3
czambelli <zmbl.soft@gmail.com>
2013-05-16 16:08:02 +0200

    Adds homepage

--- index.php
+++ index.php
@@ -3,3 +3,16 @@
 require_once 'src/autoload.php';
 
 Layout::getInstance();
+
+$page = array_key_exists('page', $_GET) ? $_GET['page'] : 'home';
+
+switch($page)
+{
+    case 'home':
+    case 'superhero':
+        require_once('src/page/' . $page . '.php');
+        exit;
+        break;
+    default:
+        //404
+}
