2f4d8b8261748a5d093da8b93f56b5199069d377
czambelli <zmbl.soft@gmail.com>
2013-05-21 12:56:37 +0200

    Adds footer, header, and several CSS fixes

--- src/page/home.php
+++ src/page/home.php
@@ -8,12 +8,10 @@ $heroes = $parser->getAll();
 ?>
 
 <ul class="super-hero-list">
-	<?php foreach ($heroes as $hero): ?>
-		<li>
-			<a href="index.php?page=superhero&amp;id=<?php echo $hero->id; ?>">
-				<img src="assets/<?php echo $hero->picture; ?>" alt="<?php $hero->nickname; ?>" />
-				<h2><?php echo $hero->nickname; ?></h2>
-			</a>
-		</li>
-	<?php endforeach; ?>
+	<?php foreach ($heroes as $hero): ?><li>
+		<a href="index.php?page=superhero&amp;id=<?php echo $hero->id; ?>">
+			<img src="assets/<?php echo $hero->picture; ?>" alt="<?php $hero->nickname; ?>" />
+			<h2><?php echo $hero->nickname; ?></h2>
+		</a>
+	</li><?php endforeach; ?>
 </ul>
