47c005478841b39f47d273379c95ad5846b287ab
czambelli <zmbl.soft@gmail.com>
2013-05-21 14:33:26 +0200

    Adds title to <a>

--- src/page/home.php
+++ src/page/home.php
@@ -9,7 +9,7 @@ $heroes = $parser->getAll();
 
 <ul class="super-hero-list">
 	<?php foreach ($heroes as $hero): ?><li>
-		<a href="index.php?page=superhero&amp;id=<?php echo $hero->id; ?>">
+		<a href="index.php?page=superhero&amp;id=<?php echo $hero->id; ?>" title="Accéder à la page descriptive de <?php echo $hero; ?>">
 			<img src="assets/<?php echo $hero->picture; ?>" alt="<?php $hero->nickname; ?>" />
 			<h2><?php echo $hero->nickname; ?></h2>
 		</a>
