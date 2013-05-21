9023c0f1e11f6325446b6051e4effb3eb3976955
czambelli <zmbl.soft@gmail.com>
2013-05-21 09:46:38 +0200

    Adds menu items

--- src/page/home.php
+++ src/page/home.php
@@ -10,10 +10,10 @@ $heroes = $parser->getAll();
 <ul class="super-hero-list">
 	<?php foreach ($heroes as $hero): ?>
 		<li>
-			<div class="profile-pic">
-				<img src="<?php echo $hero->picture; ?>" alt="<?php $hero->nickname; ?>" style="width: 128px;"/>
-			</div>
-			<h2><?php echo $hero->nickname; ?></h2>
+			<a href="index.php?page=superhero&amp;id=<?php echo $hero->id; ?>">
+				<img src="assets/<?php echo $hero->picture; ?>" alt="<?php $hero->nickname; ?>" />
+				<h2><?php echo $hero->nickname; ?></h2>
+			</a>
 		</li>
 	<?php endforeach; ?>
 </ul>
